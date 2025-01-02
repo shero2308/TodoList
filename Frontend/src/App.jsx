40dfc6dad535157330dd55b3e1831b633ef26577
shero <rakshitkumar682@gmail.com>
2025-01-02 18:12:40 +0530

    jung

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { useState , useEffect } from 'react';
 
 import { get_todo } from './api/endpoints.jsx';
 
-import Title from './components/Title.jsx'
+import Title from './components/Title.jsx';
 import TodoList from './components/TodoList.jsx';
 
 function App() {
@@ -12,7 +12,12 @@ function App() {
   const [todo , setTodo]= useState([]);
 
   useEffect(()=>{
-
+    const fetchtodo = async () =>{
+      const todo = await get_todo();
+      setTodo(todo)
+      console.log(todo)
+    }
+    fetchtodo();
   },[])
 
   return (
@@ -20,10 +25,9 @@ function App() {
       <Flex minH='100vh' w='100vw' bg='#272727' justifyContent='center'>
         <VStack w='92%' maxW='1000px' mt='60px'>
           <Title />
-          <TodoList />
+          <TodoList todo={todo} />
         </VStack>
       </Flex>
-
   )
 }
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:5173"
 ]
 
 ROOT_URLCONF = 'backend.urls'
