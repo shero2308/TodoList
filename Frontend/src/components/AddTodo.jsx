d5c65e3fdb939839da4131bfdb7caaa0edf589e7
shero <rakshitkumar682@gmail.com>
2025-01-04 17:42:20 +0530

    UPDART

--- Frontend/src/components/AddTodo.jsx
+++ Frontend/src/components/AddTodo.jsx
@@ -1,11 +1,19 @@
 import { HStack , Input  } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
+import { useState } from "react";
 
 function AddTodo(){
+
+    // const[todo_name , setTodoName] = useState('');
+
+    // handleAddTodo = ()=>{
+    //     //addTodo(todo_name)
+    // }
+
     return(
         <HStack w='100%' mt='40px' gap='0'>
-            <Input bg='#383737' border='1px solid' borderRadius='8px' borderColor='#A7A2A2' p='20px 20px' />
-            <Button></Button>
+            <Input placeholder='Add TODO here...' bg='#383737' border='1px solid' borderRadius='8px 0 0 8px' borderColor='#A7A2A2' p='20px 20px' />
+            <Button border='1px solid' borderRadius='0 8px 8px 0' bg='#0A0079' borderColor='#A7A2A2' p='20px 20px' color='white'_hover={{bg:'#1000C0'}}>Add</Button>
         </HStack>
     )
 }
