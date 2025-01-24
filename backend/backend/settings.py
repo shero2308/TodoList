23be02ec97d9933cbaa7c01c6ee2e5c58a5dd2b9
shero <rakshitkumar682@gmail.com>
2025-01-24 15:43:11 +0530

    cors_allowed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,9 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
-]
+CORS_ALLOWED_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
